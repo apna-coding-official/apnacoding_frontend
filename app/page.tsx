32e8e80c2b57011a0eea4fcfd8eb02e601e27284
Shubhashish-Chakraborty <shubhashish147@gmail.com>
2025-05-13 11:40:23 +0530

    File structure corrected!

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { motion } from "framer-motion";
-import Navbar from "./components/ui/Navbar";
+import Navbar from "../components/ui/Navbar";
 
 export default function Home() {
     return (
